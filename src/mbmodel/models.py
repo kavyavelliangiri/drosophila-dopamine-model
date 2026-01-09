45a5982f1b2e9d8c1ce2d9885c1c57446d9552e8
Kavya Velliangiri <kavyavelliangiri@Kavyas-MacBook-Pro-2.local>
2026-01-09 16:51:35 -0500

    added requirements

--- src/mbmodel/models.py
+++ src/mbmodel/models.py
@@ -3,3 +3,7 @@ __credits__ = ["Kavya Velliangiri"]
 __license__ = "MIT"
 __version__ = "v1"
 __maintainer__ = "Kavya Velliangiri"
+
+import numpy as np 
+from mbmodel.utils import relu, sigmoid, euler_step, Recorder
+
